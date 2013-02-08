c9d250c892857fdcfeec72cf634fe6ea6b8e2da7
Brandon Jahner <brandon.jahner@arestravelinc.com>
2013-02-08 09:29:11 -0800

    finished client

---
+++ app/models/Layout.scala
@@ -0,0 +1,13 @@
+package models
+
+import play.api.Play.current
+import com.novus.salat._
+import com.novus.salat.dao._
+import com.novus.salat.annotations._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+import com.novus.salat.global._
+
+object LayoutObj {
+  
+}
